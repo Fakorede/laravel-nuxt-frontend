5e7a4e4d793d503b7af9494d6fffa06f486a236d
fakorede <abiolafakorede@gmail.com>
2019-01-19 22:23:21 +0100

    showing all topics on the frontend

--- pages/topics/index.vue
+++ pages/topics/index.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="container">
         <h2>Latest Topics</h2>
-        <pre>{{topics}}</pre>
+        <div class="bg-light mt-5 mb-5" v-for="(topic, index) in topics" :key="index" style="padding: 20px;">
+            <h2>{{topic.title}}</h2>
+            <p class="text-muted">{{topic.created_at}} by {{topic.user.name}}</p>
+            <div class="ml-5 content" v-for="(content, index) in topic.posts" :key=index>
+                {{content.body}}
+                <p class="text-muted">{{content.created_at}} by {{content.user.name}}</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -22,4 +29,8 @@ export default {
 </script>
 
 <style>
+.content {
+  border-left: 10px solid #fff;
+  padding: 0 10px 0 10px;
+}
 </style>

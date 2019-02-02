40600c25b55dc2e69da8fc864db67c5f0020fe98
fakorede <abiolafakorede@gmail.com>
2019-02-02 07:55:02 +0100

    added font awesome cdn

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,11 @@ module.exports = {
         rel: "stylesheet",
         href:
           "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
       }
     ],
     script: [
